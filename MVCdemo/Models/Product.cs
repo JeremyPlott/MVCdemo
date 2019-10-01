17a11c340b96dbbf177a615c5c4ebd4ae4fef76e
Jeremy <JeremyPlott@gmail.com>
2019-10-01 14:38:51 -0400

    Add project files.

---
+++ MVCdemo/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCdemo.Models {
+
+    public class Product {
+
+        public int ProductId { get; set; }
+
+        [StringLength(80)]
+        public string Description { get; set; }
+
+        [Range(0,1000)]
+        public int QtyOnHand { get; set; }
+
+        [Range(0,1000)]
+        public int? ReorderPoint { get; set; }
+
+        [StringLength(8)]
+        public string Status { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal Cost { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ MVCdemo/Models/Vendor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCdemo.Models {
+
+    public class Vendor {
+
+        public int VendorId { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        public string Status { get; set; }
+
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$")]
+        [Required]
+        public string Phone { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string Email { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
